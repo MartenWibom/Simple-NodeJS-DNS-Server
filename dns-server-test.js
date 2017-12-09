1a97c069c050de3d65b76ab11d419c02a43871e0
Mårten Wibom <marten.wibom@gmail.com>
2017-12-09 22:37:25 +0100

    First commit

--- dns-server-test.js
+++ dns-server-test.js
@@ -3,20 +3,10 @@ var dns = require('native-dns');
 var util = require('util');
 
 var customEntries = {
-	'test.dns': [
+	'ha-ui': [
 		{
-			name: 'test.dns',
-			address: '127.1.2.1',
-			ttl: 30
-		},
-		{
-			name: 'test.dns',
-			address: '127.1.2.2',
-			ttl: 30
-		},
-		{
-			name: 'test.dns',
-			address: '127.1.2.3',
+			name: 'ha-ui',
+			address: '192.168.2.201',
 			ttl: 30
 		}
 	]
@@ -26,23 +16,20 @@ var server = dns.createServer();
 
 server.on('request', function (request, response) {
 	var domain = request.question[0].name;
-	if(customEntries[domain]){
+	if (customEntries[domain]) {
 		//if custom entry exists, push it back...
-		var entries = customEntries[domain];
-		for(var i=0;i<entries.length;i++){
-			var entry = entries[i];
-			response.answer.push(dns.A(entry));
-		}
+		customEntries[domain].map((entry) => response.answer.push(dns.A(entry)));
 		response.send();
 	} else {
+		// OK, not found. continue upwards
 		var question = dns.Question({
-		  name: domain,
-		  type: 'A',
+			name: domain,
+			type: 'A',
 		});
 		var req = dns.Request({
-		  question: question,
-		  server: { address: '8.8.8.8', port: 53, type: 'udp' },
-		  timeout: 1000,
+			question: question,
+			server: { address: '8.8.8.8', port: 53, type: 'udp' },
+			timeout: 1000,
 		});
 		req.on('message', function (err, answer) {
 			var entries = [];
@@ -56,8 +43,8 @@ server.on('request', function (request, response) {
 });
 
 server.on('error', function (err, buff, req, res) {
-  console.log(err.stack);
+	console.log(err.stack);
 });
 
-console.log('Listening on '+53);
+console.log('Listening on ' + 53);
 server.serve(53);
